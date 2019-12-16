b94c21141a3b0a1048bec50ac79bd4eb78841ee8
95dewadew <41128556+95dewadew@users.noreply.github.com>
2019-12-16 10:06:53 +0700

    Update wg.sh

--- wg.sh
+++ wg.sh
@@ -121,6 +121,8 @@ PublicKey = $(cat tempubkey)
 AllowedIPs = 10.0.0.$newnum/32
 EOF
     wg set wg0 peer $(cat tempubkey) allowed-ips 10.0.0.$newnum/32
+    qrencode -t ansiutf8  < /etc/wireguard/$newname.conf
+    qrencode -o $userdir/$user.png  < /etc/wireguard/$newname.conf
     echo -e "Add complete, file directory：/etc/wireguard/$newname.conf"
     rm -f temprikey tempubkey
 }
