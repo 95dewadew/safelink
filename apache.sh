f875b6eb42a8862b07af68183371526549ada70b
95dewadew <41128556+95dewadew@users.noreply.github.com>
2020-01-05 16:42:12 +0700

    Create apache.sh

---
+++ apache.sh
@@ -0,0 +1,13 @@
+# install webserver
+cd
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/95dewadew/ooo/master/conf/nginx.conf"
+sed -i 's/www-data/nginx/g' /etc/nginx/nginx.conf
+mkdir -p /root/users/
+echo "<pre><center><img src="http://jagoanssh.com/favicon.png" data-original-height="120" data-original-width="120" height="320" width="320" /></a></center><br><center><font color="red" size="50"> SETUP BY: JUCKY VENGEANCE</b></font></center><center><br><font color="blue" size="50"> WA: 083898587500</b></font></center></pre>" > /home/vps/public_html/index.html
+echo "<?php phpinfo(); ?>" > /root/users/info.php
+rm /etc/nginx/conf.d/*
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/95dewadew/ooo/master/conf/vps.conf"
+sed -i 's/apache/nginx/g' /etc/php-fpm.d/www.conf
+chmod -R +rx /root/users/
+service php-fpm restart
+service nginx restart
