025025c2d6bae7202e93b9cfd823868f8200dd57
95dewadew <41128556+95dewadew@users.noreply.github.com>
2020-06-28 04:55:03 +0700

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -3,7 +3,7 @@ cd
 wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/95dewadew/ooo/master/conf/nginx.conf"
 sed -i 's/www-data/nginx/g' /etc/nginx/nginx.conf
 mkdir -p /root/users/
-echo "<pre><center><img src="http://jagoanssh.com/favicon.png" data-original-height="120" data-original-width="120" height="320" width="320" /></a></center><br><center><font color="red" size="50"> SETUP BY: JUCKY VENGEANCE</b></font></center><center><br><font color="blue" size="50"> WA: 083898587500</b></font></center></pre>" > /home/vps/public_html/index.html
+echo "<pre><center><img src="http:///favicon.png" data-original-height="120" data-original-width="120" height="320" width="320" /></a></center><br><center><font color="red" size="50"> SETUP BY: JUCKY VENGEANCE</b></font></center><center><br><font color="blue" size="50"> WA: 083898587500</b></font></center></pre>" > /home/vps/public_html/index.html
 echo "<?php phpinfo(); ?>" > /root/users/info.php
 rm /etc/nginx/conf.d/*
 wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/95dewadew/ooo/master/conf/vps.conf"
